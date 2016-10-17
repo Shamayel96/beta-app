161a2fdacc9cea81314a56bebfc1c928f6eb4e3c
Amy Kamin <akamin@hawk.iit.edu>
2016-10-17 16:14:52 -0500

    create model validations and associations

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,5 @@
 class Country < ApplicationRecord
+  validates :country, presence: true
+  validates_length_of :country, :within => 1..40
+  has_many :dishes
 end
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,2 +1,6 @@
 class Dish < ApplicationRecord
+  validates :title, presence: true
+  validates_length_of :title, :within => 1..25
+  validates :recipe, presence: true
+  belongs_to :country
 end
