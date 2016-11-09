d392561cee3331f5fd295fc65953170310882ec8
Amy Kamin <akamin@hawk.iit.edu>
2016-11-09 12:58:12 -0600

    Ran bundle install & image handler to model

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -3,4 +3,7 @@ class Dish < ApplicationRecord
   validates_length_of :title, :within => 1..25
   validates :recipe, presence: true
   belongs_to :country
+  
+  has_attached_file :image, styles: { arge: "600x600>", medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
