c9542cf038791218707ab6fdb17b2dd42ffa0bcf
Amy Kamin <akamin@hawk.iit.edu>
2016-11-17 20:36:25 -0600

    added session destroy method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_or_create_from_auth_hash(auth_hash)
-    self.current_user = @user
-    redirect_to '/'
+    # render text: request.env['omniauth.auth'].to_yaml
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+    session[:id] = @user.id
+    redirect_to countries_path
   end
 
-  protected
-
-  def auth_hash
-    request.env['omniauth.auth']
+  def destroy
+    reset_session
+    redirect_to countries_path
   end
 end
